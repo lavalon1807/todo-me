5504b69c985d7dcac6559f60602c00345f19c15f
Egor Limachev <le@sinergium.ru>
2026-03-24 10:24:53 +0300

    стилизован и адаптирован ленд, без скрытых блоков

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import "./App.css";
 import Author from "./components/sidebar/Author";
 import Navigations from "./components/sidebar/Navigations";
-import SetTasks from "./components/first-screen/SetTasks";
-import Task from "./components/first-screen/Task";
+import SetTasks from "./components/MainPage/SetTasks";
+import Task from "./components/MainPage/Task";
 
 function App() {
     return (
@@ -12,7 +12,7 @@ function App() {
                 <Author />
                 <Navigations />
             </div>
-            <div class="main__page">
+            <div class="main__page wrapper">
                 <SetTasks />
                 <Task />
             </div>
